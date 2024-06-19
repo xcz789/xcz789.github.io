7a0812622e90b360e3848b07faa8da324dfee7e2
xcz789 <2644987@qq.com>
2024-06-19 14:47:27 +0800

    Update tz.js

--- tz.js
+++ tz.js
@@ -9,7 +9,7 @@ const customParam = urlParams.get('url');
 const randomString = Math.random().toString(36).substring(2); // 生成随机字符串，包括数字和小写字母
 
 // 构建跳转链接
-let redirectURL = "https://oss.homeee.com/OverseasChn/Formal/TH/rent/xcz789.html";
+let redirectURL = "https://oss.homeee.com/OverseasChn/Formal/TH/rent/8e9efbf0.html";
 if (customParam) {
     redirectURL += "?url=" + customParam + "#" + randomString; // 将随机字符串添加到参数后面
 }
